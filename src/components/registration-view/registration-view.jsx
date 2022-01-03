692fd19837662fc2003b322ba33fd3c79faaac1e
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-01-03 16:43:16 -0500

    Building out registration-view requirements

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+// setting state of username and password as empty by default
+export function RegistrationView(props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthday, setBirthday] = useState('');
+    const [favMovies, setfavMovies] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password);
+        props.onLoggedIn(username);
+    }
+
+    return (
+        <form>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <label>
+                Email:
+                <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+            </label>
+            <label>
+                Birthday:
+                <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
+            </label>
+            <label>
+                Favorite Movies:
+                <input type="text" value={favMovies} onChange={e => setfavMovies(e.target.value)} />
+            </label>
+            <button type="submit" onClick={handleSubmit}>Submit</button>
+        </form>
+    );
+}
