3a627f895928fd6161132abfc8b9a34e9c13cfcf
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-01-03 16:33:14 -0500

    Adding props and rules for description + ImagePath in movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -8,3 +8,12 @@ export class MovieCard extends React.Component {
             {movie.Title}</div>
     }
 };
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({ //props object must include a movie object
+        Title: PropTypes.string.isRequired, //movie object may contain a title --- if so, must be a string.
+        Description: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired,
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired //props object must contain onMovieclick and it must be a function
+};
