2d7acba8e5e0638a6bcd6388dba4b2e91c81e675
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-01-10 08:59:21 -0500

    Adding sizing to login-view form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { RegistrationView } from '../registration-view/registration-view';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Card from 'react-bootstrap/Card';
+import { Navbar, Container, Nav, Form, Button, Card, Container, Row, Col } from 'react-bootstrap';
 
 
 
@@ -23,31 +19,31 @@ export function LoginView(props) {
     return (
         <Container>
             <Row>
-                <Row>
-                    <Col></Col>
-                    <Col>
-                        <Card>
-                            <Card.Body>
-                                <Form>
-                                    <Form.Group controlId="formUsername">
-                                        <Form.Label>Username:</Form.Label>
-                                        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-                                    </Form.Group>
+                <Col></Col>
+                <Col>
+                    <Card style={{ marginTop: 100, marginBottom: 100, width: 300 }}>
+                        <Card.Body>
+                            <Card.Title style={{ textAlign: 'center' }} >Welcome to myFlix!</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted text-center">*Please Login*</Card.Subtitle>
+                            <Form>
+                                <Form.Group controlId="formUsername">
+                                    <Form.Label>Username:</Form.Label>
+                                    <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                                </Form.Group>
 
-                                    <Form.Group controlId="formPassword">
-                                        <Form.Label>Password:</Form.Label>
-                                        <Form.Control type="text" onChange={e => setPassword(e.target.value)} />
-                                    </Form.Group>
-                                    <Button variant="primary" type="submit" onClick={handleSubmit}>
-                                        SubmitSSSSSSSSS
-                                    </Button>
-                                </Form>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
+                                <Form.Group controlId="formPassword">
+                                    <Form.Label>Password:</Form.Label>
+                                    <Form.Control type="text" onChange={e => setPassword(e.target.value)} />
+                                </Form.Group>
+                                <Button variant="primary" type="submit" onClick={handleSubmit}>
+                                    Submit!
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col></Col>
             </Row>
         </Container>
-
     );
 }
